ea126795870158625ff90c4b339859a7fe2221c4
tornikeo <tornikeo@users.noreply.github.com>
2022-02-03 23:34:23 +0000

    deploy: 639fb461cd9103846f8865a2aff6536fed749a18

--- _nuxt/389d2c4.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{269:function(n,e,t){"use strict";t.r(e);var l=t(61),component=Object(l.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("h1",[n._v("Aloha!")])}),[],!1,null,null,null);e.default=component.exports}}]);

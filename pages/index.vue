77ac8578bbb3691d35986489fe41876f9ee700fa
tornikeo <tornikeonoprishvili@gmail.com>
2022-02-04 03:03:46 +0400

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+// <template>
+//   <Tutorial/>
+// </template>
+
+<!-- <template>
+  <nuxt-link to="/">Home page</nuxt-link>    
+</template> -->
+<template>
+  <main>
+    <h1>Home page</h1>
+    <NuxtLink to="/about">
+      About (internal link that belongs to the Nuxt App)
+    </NuxtLink>
+    <a href="https://nuxtjs.org">External Link to another page</a>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+
+</script>
